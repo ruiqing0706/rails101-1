3674cb080620daa23a699ba4c4e5d81c826654e4
ruiqing0706 <2944925501@qq.com>
2016-12-17 10:22:32 +0800

    when created a group,join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -59,7 +60,7 @@ class GroupsController < ApplicationController
     else
       flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
     end
-    redirect_to group_path(@group)    
+    redirect_to group_path(@group)
   end
 
   private
